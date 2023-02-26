60fbfa7d0a4777d8ee2fdd1a65e9d6fc37729712
Shishira4 <shivashishira@gmail.com>
2023-02-26 22:15:10 +0530

    Tried controllers

---
+++ app/controllers/department.controller.js
@@ -0,0 +1,37 @@
+const { condition} = require("sequelize");
+const db = require("../models");
+const department = db.department;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) =>{
+    const {
+        departmentid,
+        departmentname,
+        description,
+        status
+    } = req.body
+    const data2 = {
+        departmentid,
+        departmentname,
+        description,
+        status
+    };
+    //save address
+    return department.create(data2)
+        .then(async data) => {
+            data = data.get({plain: true});
+
+            return res.status(201).json({
+                code:200,
+
+            })
+        }
+    //retreiving addresss
+    exports.findAll = (req, res) => {
+        const limit = Math.min(parseInt(req.query.limit || 10), 10);
+        const condition = {}
+        if(req.query.department) {
+            condition.
+        }
+    }
+}
---
+++ app/controllers/doctor.controller.js
@@ -0,0 +1,55 @@
+const { condition} = require("sequelize");
+const db = require("../models");
+const doctor = db.doctor;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) =>{
+    const {
+        doctorid,
+        doctorname,
+        mobileno,
+        departmentid,
+        loginid,
+        password,
+        status,
+        education,
+        experience,
+        consultancy_charge,
+        doc_sign,
+        designation,
+        reg_no
+    } = req.body
+    const data3 = {
+        doctorid,
+        doctorname,
+        mobileno,
+        departmentid,
+        loginid,
+        password,
+        status,
+        education,
+        experience,
+        consultancy_charge,
+        doc_sign,
+        designation,
+        reg_no
+    };
+    //save address
+    return doctor.create(data3)
+        .then(async data) => {
+            data = data.get({plain: true});
+
+            return res.status(201).json({
+                code:200,
+
+            })
+        }
+    //retreiving addresss
+    exports.findAll = (req, res) => {
+        const limit = Math.min(parseInt(req.query.limit || 10), 10);
+        const condition = {}
+        if(req.query.department) {
+            condition.
+        }
+    }
+}
