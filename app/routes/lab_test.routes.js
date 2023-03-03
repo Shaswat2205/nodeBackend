70a2f2d9c0702879437f27f87ebcd5337b37edb5
Shishira4 <shivashishira@gmail.com>
2023-03-03 12:16:50 +0530

    debugging lab test controller

--- app/routes/lab_test.routes.js
+++ app/routes/lab_test.routes.js
@@ -3,5 +3,5 @@ const lab_test = require("../controllers/lab_test.controller")
 module.exports = app => {
     const router = require("express").Router();
     router.post("/",lab_test.create);
-    app.use("api/lab_test",router);
+    app.use("/api/lab_test",router);
 };
